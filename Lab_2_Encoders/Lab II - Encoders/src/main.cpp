c4d036ff7252f84716f59a182f653390c9e73a18
Cameron Castillo <77078192+camstillo@users.noreply.github.com>
2023-04-20 18:08:47 -0800

    Slight changes

--- Lab_2_Encoders/Lab II - Encoders/src/main.cpp
+++ Lab_2_Encoders/Lab II - Encoders/src/main.cpp
@@ -7,13 +7,38 @@
 #define OUT_2   4
 
 int main(){
-  static encoder leftEncoder;
-  //bool stateChange = LOW;
+  //intialize pins
+  pinMode(IN_1, INPUT);
+  pinMode(IN_2, INPUT);
+  pinMode(OUT_1, OUTPUT);
+  pinMode(OUT_2, OUTPUT);
 
-  state* state = &Low;
-  state* prevState = state;
+  //initialize left encoder object
+  static encoder leftEncoder;
+  leftEncoder.begin(IN_1);
+  bool leftStateChange = LOW;
+  state* leftState = &Low;
+  state* leftPrevState = leftState;
+
+  //initialize right encoder object
+  static encoder rightEncoder;
+  rightEncoder.begin(IN_2);
+  bool rightStateChange = LOW;
+  state* rightState = &Low;
+  state* rightPrevState = rightState;
 
   while(1){
-  
+    //if either the state has changed, 
+    //run the state entrance procedure
+    if(leftStateChange){
+      leftState->enter();
+    } else if(rightStateChange) {
+      rightState->enter();
+    }
+
+    //otherwise, continually run the 
+    //run operation for each state
+    leftState = leftPrevState->
+
   }
 }
